fa7fa516c1206c74c36f80cae21c94bb555d6a84
Daniel Gross <danwgross@gmail.com>
2020-08-07 15:18:32 -0400

    add inventory, pos and user module skeleton

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatTooltipModule } from '@angular/material/tooltip'
 
-const materialModules = [MatButtonModule, MatToolbarModule, MatIconModule]
+const materialModules = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatTooltipModule,
+]
 
 @NgModule({
   declarations: [],
